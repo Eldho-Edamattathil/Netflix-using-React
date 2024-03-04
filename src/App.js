7ab8e5c87a6e0c20ee829f7133175516cd39cca5
Eldho George <eldhogeorgeee@gmail.com>
2024-03-04 09:31:46 +0530

    Netflix almost completed

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Banner from './components/Banner/Banner';
 import Rowpost from './components/Rowpost/Rowpost';
+import { originals,action } from './urls';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
-      <Rowpost />
+      <Rowpost url={originals} title='Netflix Originals' />
+      <Rowpost url={action} title='Actions' isSmall />
     </div>
   );
 }
---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseurl='https://api.themoviedb.org/3'
+export const API_KEY='8969d455a66ac930baa853b1caa31f00'
+export const imageUrl='https://image.tmdb.org/t/p/original'
